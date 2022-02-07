1845c45ad39f058762ccffa1415c60db6c487281
Limunard0 <josip.merda@gmail.com>
2022-02-07 22:51:56 +0100

    Rjesio dodatni zadatak

--- src/com/Jeetcode/Main.java
+++ src/com/Jeetcode/Main.java
@@ -15,16 +15,25 @@ public class Main {
             int brojkaTri = i % 3;
             int brojkaPet = i % 5;
             int brojkaDeset = i % 10;
+            int brojkaPetINula = (brojkaTri) + (brojkaPet);
 
 
-            if (brojkaTri == 0) {
+            if (brojkaPetINula == 0) {
+                System.out.println("Broj je djeljiv sa 3 i 5");
+
+
+            } else  if (brojkaTri == 0) {
                 System.out.println(i);
+
 
             } else if (brojkaDeset == 0) {
                 System.out.println("Broj je djeljiv sa 10");
 
+
             } else if (brojkaPet == 0) {
                 System.out.println(i);
+
+
             } else System.out.println("Broj nije djeljiv");
         }
     }
